ff02e2bfd2c4507cd279c48d9d5bf978d0abde9d
Summer <98126132+UmRange@users.noreply.github.com>
2022-06-11 23:20:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var Payload = `Mom dad look I made it this far :D`
 Request.post("https://discordapp.com/api/v6/channels/Target_Channel_ID/messages", {
     headers: {
         "Content-Type": "application/json",
-        "Authorization": "YourDiscordTokenHere"
+        "Authorization": "Your Discord Token here"
     },
     body: JSON.stringify({ "content": Payload })
 }, function(err, res, body){
