e12f5e842ea8dfe786006e9fe5bee2cd02aca56a
Summer <98126132+UmRange@users.noreply.github.com>
2022-06-11 23:20:14 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+
+const Request = require("request")
+
+var Payload = `Mom dad look I made it this far :D`
+
+Request.post("https://discordapp.com/api/v6/channels/Target_Channel_ID/messages", {
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": "YourDiscordTokenHere"
+    },
+    body: JSON.stringify({ "content": Payload })
+}, function(err, res, body){
+    console.log(err)
+    console.log(body)
+})
